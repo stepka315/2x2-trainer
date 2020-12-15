c9bbaa85e5541e6cd575ee28edd54f5550475f9b
stepka315 <52010212+stepka315@users.noreply.github.com>
2020-12-15 13:58:17 +0300

    Keyboards shortcuts are added

--- trainer.py
+++ trainer.py
@@ -55,15 +55,14 @@ def main(filename, subset):
     prev_scr = None
     
     
-    def generate_scr(lbl, s):
-        selection = s(vars_dict)
+    def generate_scr(lbl, selection):
         nonlocal prev_scr
         if selection:
             random_type = random.choice(list(selection.keys()))
             scr = prev_scr
             while scr == prev_scr:
                 scr = random.choice(scrambles[random_type])
-            
+
             prev_scr = scr
             
             pre_auf = random.choice(auf)
@@ -86,15 +85,28 @@ def main(filename, subset):
         for g in vars_dict:
             vars_dict[g].set(0)
     
-    
-    b = tk.Button(root, text='generate scranble', command= lambda : generate_scr(l, state), bg='white', activebackground='#93c7eb')
+    def key_events(e):
+        if e.char == ' ':
+            generate_scr(l, state(vars_dict))
+        if e.char == 'a':
+            if len(state(vars_dict)) == len(groups):
+                deselect_all()
+            else:
+                select_all()
+            
+    b = tk.Button(root, text='generate scranble', command= lambda : generate_scr(l, state(vars_dict)), bg='white', activebackground='#93c7eb')
     b.grid(sticky='w')
     tk.Button(root, text='select all', command= select_all, bg='white', activebackground='#93c7eb').grid(sticky='w')
     tk.Button(root, text='deselect all', command= deselect_all, bg='white', activebackground='#93c7eb').grid(sticky='w')
     l.grid(row=0, column=2)
+    
+    root.bind("<KeyPress>", key_events)
+    
     root.mainloop()
+         
     
 if __name__ == '__main__':
+    
     def refresh(new_file, subset):
         root.destroy()
         main(new_file, subset)
